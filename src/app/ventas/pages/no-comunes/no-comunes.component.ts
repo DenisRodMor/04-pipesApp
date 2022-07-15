44ca6801f81a68d6cfee3c228103c7ed8f38d786
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-07-15 14:59:02 -0600

    Uso de  I18nPluralPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,12 +8,23 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent  {
 
+  //i18nSelect Pipe
   nombre : string = 'Dennis';
   genero : string = 'masculino';
-
   invitacionMapa = {
     'masculino' : 'invitarlo',
     'femenino'  : 'invitarla'
   }
 
+
+  //i18nPlural Pipe
+  clientes : string [] = ['Gabriela', 'Dennis', 'Angie', 'Jesús'];
+  clientesMapa ={
+    '=0':'no tenemos ningún cliente esperando.',
+    '=1':'tenemos un cliente esperando.',
+    '=2':'tenemos dos clientes esperando.',
+    '=3':'tenemos tres clientes esperando.',
+    '=4':'tenemos cuatro clientes esperando.',
+    'other':'tenemos # clientes esperando.'
+  }
 }
